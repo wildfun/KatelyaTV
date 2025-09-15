568dc153d88864833d47bcf72d30473169450af1
wildfun <97380031+wildfun@users.noreply.github.com>
2025-09-15 16:02:56 +0800

    page.tsx
    11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ const MainKatelyaLogo = () => {
       <div className='logo-background-glow'></div>
 
       {/* 主 Logo */}
-      <div className='main-katelya-logo'>KatelyaTV</div>
+      <div className='main-katelya-logo'>Wildfin TV</div>
 
       {/* 副标题 */}
       <div className='mt-3 text-center'>
@@ -65,9 +65,9 @@ const BottomKatelyaLogo = () => {
       </div>
 
       <div className='text-center'>
-        <div className='bottom-logo'>KatelyaTV</div>
+        <div className='bottom-logo'>Wildfun TV</div>
         <div className='mt-2 text-sm text-gray-500 dark:text-gray-400 opacity-75'>
-          Powered by KatelyaTV Core
+          Powered by Wildfun TV Core
         </div>
       </div>
     </div>
